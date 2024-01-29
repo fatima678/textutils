b4c6f0e78efddd4c3de71df525a8fbdd6fce3445
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-01-29 23:48:22 +0500

    Enable dark mode on navbar
    Enable dark mode on navbar using states

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ import TextForm from './components/TextForm';
 // import About from './components/About';
 
 function App() {
-   const  [mode, setmode] = useState("dark");
+   const  [mode, setmode] = useState("light");
+   const toggleMode = ()=>{
+      if(mode==='light'){
+        setmode('dark') 
+
+      }
+      else{
+        setmode('light')
+      }
+   }
 
    
   return (
     <>
-    <Navbar title="TextUtils" mode={mode}/>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     
     <div className='container my-3' >
       {/* <TextForm heading='Enter the text to analyze below'/> */}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-    <nav className={`navbar navbar-expand-lg $ navbar-${props.mode} bg-dark`}>
+    <nav className={`navbar navbar-expand-lg $ navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
       <a className="navbar-brand" href="/">{props.title}</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,12 +19,12 @@ export default function Navbar(props) {
           </li>
          
         </ul>
-        <div class="form-check form-switch text-light">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-        <label class="form-check-label" for="flexSwitchCheckDefault">Enable Dark Mode</label>
+        <div className={`form-check form-switch text-${props.mode==='light'?`dark`:'light'}`}>
+        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+        <label className="form-check-label" htmlfor="flexSwitchCheckDefault">Enable Dark Mode</label>
 </div>
     
-      </div>
+      </div> 
     </div>
   </nav>
   )
